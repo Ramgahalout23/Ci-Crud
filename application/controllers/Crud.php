7c596a0978821020e302bc4e092a7456abdf0a8f
ramkumar <ramkumargahalout67@gmail.com>
2020-03-20 02:36:08 +0530

    First commit Message

--- application/controllers/Crud.php
+++ application/controllers/Crud.php
@@ -5,6 +5,15 @@ class Crud extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('Crud_view');
+		$this->load->database();  
+         //load the model  
+         $this->load->model('Crud_model');  
+         //load the method of model  
+         $data['products']=$this->Crud_model->getAllproducts();  
+         //return the data in view  
+// print_r($products);
+// die();
+		$this->load->view('Crud_view', $data);
 	}
 }
+?>
